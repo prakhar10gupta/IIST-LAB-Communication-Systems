08d2e94b3c1cab9c40c16415e071df0cba87ae80
Prakhar Gupta <66132869+prakhar10gupta@users.noreply.github.com>
2022-02-23 00:11:14 +0530

    Delete powerSpectrum.m

--- powerSpectrum.m
+++
@@ -1,6 +0,0 @@
-function [powerSpec, f] = powerSpectrum(tdSignal, fs)
-N = length(tdSignal); % length of baseband signal
-Xf = fft(tdSignal, N);
-f = fs/N *(0: 1: N-1); % frequency axis for DFT
-powerSpec = abs(Xf).^2 / N;
-end
